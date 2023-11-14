d1fd968d53974dfb82f9eef4f50aed9bd25299e7
AbyG1 <abygeorge133@gmail.com>
2023-11-14 08:46:13 +0530

    Setup add task functionality

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,5 +1,34 @@
-const button = document.querySelector('btn');
+const button = document.querySelector('.btn')
+const input = document.getElementById('inputText')
+const container = document.getElementById('list')
+
+
+
+
+
+
+
+
 
 button.addEventListener('click',() => {
-    
+    if(input.value == ''){
+        alert('Enter something on the input box');
+        return
+    }
+
+    const todoItem = document.createElement('div')
+    const editButton = document.createElement('button')
+    const deleteButton = document.createElement('button')
+    todoItem.textContent = input.value;
+
+    editButton.innerHTML ='<i class="bi bi-pencil-square"></i>'
+    deleteButton.innerHTML ='<i class="bi bi-trash3"></i>'
+
+
+    container.appendChild(todoItem)
+    todoItem.appendChild(editButton)
+    todoItem.appendChild(deleteButton)
+
+    input.value= ' '
+
 })
