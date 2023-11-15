42fc765612025163316df54b3e12aca391eb2188
AbyG1 <abygeorge133@gmail.com>
2023-11-15 23:51:35 +0530

    Setup home page desing

--- login-sample/home.js
+++ login-sample/home.js
@@ -3,12 +3,6 @@ const uname = document.getElementById('name');
 uname.textContent = username;
 
 
-//ivide teacher h1 tag id koduth
-// id veche target cheythu, innerhtml vechu matti
-//
-//
-//
-//
 
 function logout() {
     localStorage.clear();
