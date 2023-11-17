efb0c439efc49edfae256a85c29016d77b4e078b
AbyG1 <abygeorge133@gmail.com>
2023-11-17 10:37:35 +0530

    Add simple Validation to enter deatils

--- car-model/script.js
+++ car-model/script.js
@@ -7,10 +7,23 @@ const carPrice = document.getElementById('price')
 const carKey = document.getElementById('key')
 
 function storeRecord() {
-    const car = {
-        'key' : carKey.value,
-        'brand': carBrand.value,
-        'price': carPrice.value
+    if (carBrand.value.trim() === '' || carPrice.value.trim() === '' || carKey.value.trim() === '') {
+        alert('Fill in all details');
+        return;
+    } else {
+
+    if(carKey.value in localStorage){
+        alert('Key already exist in records')
+    } else {
+        const car = {
+            'key' : carKey.value,
+            'brand': carBrand.value,
+            'price': carPrice.value
+        }
+        localStorage.setItem(car.key,JSON.stringify(car))
+
+
     }
-    localStorage.setItem(car.key,JSON.stringify(car))
+            
+}
 }
